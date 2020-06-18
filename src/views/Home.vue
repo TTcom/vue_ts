61bc11526f82f8c32ed8b6f8292e6efde3e6b123
Your Name <you@example.com>
2020-06-18 19:48:51 +0800

    addsomthing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,21 @@
       <li v-for="(item, index) in data" :key="index">{{ item }}</li>
     </ul>
     <HelloWorld :msg="txt" @fromHome="fromHome" />
+    <button type="button" @click="addcount">count++</button>
+    <div>
+      {{ plusOne }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from "@vue/composition-api";
+import {
+  defineComponent,
+  reactive,
+  ref,
+  toRefs,
+  computed
+} from "@vue/composition-api";
 import HelloWorld from "../components/HelloWorld.vue";
 export default defineComponent({
   name: "Home",
@@ -23,25 +33,45 @@ export default defineComponent({
       //创建一个接口
       arr: any[];
       name: string;
-      def: number;
+      def?: number;
     };
     const state = reactive<MyObject>({
       arr: [1, 2, 3, 125],
-      name: "",
-      def: 121
+      name: ""
     });
+    const count = ref(1);
     const methods = {
       fromHome(val: string) {
         console.log(val);
         txt.value = val;
+      },
+      sumValue<T>(value: T): T {
+        //传入什么类型就返回什么类型
+        return value;
+      },
+      addcount() {
+        count.value++;
+        const kk = methods.sumValue<number>(count.value); //指定函数传入的参数类型
+        const run = (val: number): void => {
+          //void没有任何返回值
+          if (val > 2) {
+            console.log(11);
+          }
+        };
+        run(56);
+        console.log("kk", kk);
       }
     };
+
+    const plusOne = computed(() => count.value + 1);
     return {
       txt,
+      count,
       length,
       data,
       ...toRefs(state),
-      ...methods
+      ...methods,
+      plusOne
     };
   },
   components: {
